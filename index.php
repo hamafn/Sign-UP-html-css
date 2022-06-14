8cc79b0b9553570bc842d5093f17af27aa8986d7
hamafn <60230070+hamafn@users.noreply.github.com>
2022-06-14 07:53:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+   ob_start();
+   session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+<link href="style.css" rel="stylesheet">
+<title>By Mr Nobody </title>
+</head>
+<body>
+
+<p> <a href="https://t.me/mrnobdy_090">&copy;Mr Nobody</a> 
+</p>
+</form>
+<script type="text/javascript">alert("Telegram:MrNobody_090");</script>
+</body>
+</html>
